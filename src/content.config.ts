4cdab636ba1c3ab63240922900479b1f6abd9445
Rhys Turner <rhys.turnstylez@gmail.com>
2026-07-29 18:39:51 +0800

    add: july and august

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,6 +14,7 @@ const blog = defineCollection({
 		heroImage: z.string().optional(),
 		author: z.string().optional(),
 		tags: z.array(z.string()).optional(),
+			lumaEventId: z.string().optional(),
 		draft: z.boolean().optional(),
 	}),
 });
